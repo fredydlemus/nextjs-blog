ac51cb75abcb3eab82c4081ca63fd658f5a79f75
Fredy Daniel Flores Lemus <56806728+fredydlemus@users.noreply.github.com>
2023-04-17 15:12:17 -0600

    redesign blog

--- components/header.js
+++ components/header.js
@@ -1,43 +1,70 @@
+import { useState } from "react";
 import styles from "./header.module.css";
 import Link from "next/link";
 
+const categories = [
+  {
+    name: "Inicio",
+    slug: "/",
+  },
+  {
+    name: "Angular",
+    slug: "/angular",
+  },
+];
+
 export default function Header() {
+  const [isMenuToggleOpen, setMenuToggleOpen] = useState(false);
+
+  function toggleMenu() {
+    setMenuToggleOpen(!isMenuToggleOpen);
+  }
+
   return (
     <header className={styles.siteHeader}>
-      <div className={styles.siteLogo}>
-        <Link href="/">
-          <img src="/images/binary-code.png" alt="Logo del sitio" />
-        </Link>
-      </div>
-      <button className={styles.menuToggle}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-      <nav className={styles.siteNavigation}>
-        <ul>
-          <li>
-            <Link href="#">Inicio</Link>
-          </li>
-          <li>
-            <Link href="#">Categoria 1</Link>
-          </li>
-          <li>
-            <Link href="#">Categoria 2</Link>
-          </li>
-          <li>
-            <Link href="#">Categoria 3</Link>
-          </li>
-          <li>
-            <Link href="#">Categoria 4</Link>
-          </li>
-        </ul>
-      </nav>
-      <div className={styles.siteSearch}>
-        <form>
-          <input type="text" placeholder="Buscar..." />
-          <button type="submit">Buscar</button>
-        </form>
+      <div className={styles.siteContainer}>
+        <div className={styles.siteLogo}>
+          <Link href="/">
+            <h4>fredydlemus.blog</h4>
+          </Link>
+        </div>
+        <button onClick={toggleMenu} className={styles.menuToggle}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        {isMenuToggleOpen && (
+          <div className={styles.siteToggleNavigation}>
+            <div className={styles.siteSearchToggle}>
+              <form>
+                <input type="text" placeholder="Buscar..." />
+                <button type="submit">Buscar</button>
+              </form>
+            </div>
+            <ul>
+              {categories.map((category) => (
+                <li>
+                  <Link href={category.slug}>{category.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <nav className={styles.siteNavigation}>
+          <ul>
+            {categories.map((category) => (
+              <li>
+                <Link href={category.slug}>{category.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className={styles.siteSearch}>
+          <form>
+            <input type="text" placeholder="Buscar..." />
+            <button type="submit">Buscar</button>
+          </form>
+        </div>
       </div>
     </header>
   );
