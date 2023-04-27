099eb6894224fddb8860cde6155693ed426ab767
Fredy Daniel Flores Lemus <hello@fredydlemus.com>
2023-04-27 10:36:02 -0600

    Revert "New change"

--- components/layout.js
+++ components/layout.js
@@ -8,11 +8,7 @@ import Footer from "./footer";
 import { links } from "../lib/links";
 
 const name = "Fredy Flores";
-<<<<<<< HEAD
-export const siteTitle = "fredydlemus";
-=======
 export const siteTitle = "fredydlemus.blog";
->>>>>>> 3775fad7ab57d712a75452d639846a83e87b698c
 
 export default function Layout({ children, home }) {
   return (
@@ -21,44 +17,7 @@ export default function Layout({ children, home }) {
       <Head>
         <link rel="icon" href="/images/binary-code.png"></link>
       </Head>
-<<<<<<< HEAD
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/fredydlemus.jpeg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2X1}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <Image
-                priority
-                src="/images/fredydlemus.jpeg"
-                className={utilStyles.borderCicle}
-                height={108}
-                width={108}
-                alt={name}
-              />
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
-                {name}
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
-      <main>{children}</main>
-=======
       {children}
->>>>>>> 3775fad7ab57d712a75452d639846a83e87b698c
       {!home && (
         <div className={styles.backToHome}>
           <Link href="/">Regresar al inicio</Link>
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,6 @@
 import "../styles/globals.css";
 import "prismjs/themes/prism-okaidia.css";
-import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
