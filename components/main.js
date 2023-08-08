36d8d3524c4e97609550d08148775751eed01d83
fredydlemus <fredyflemus@gmail.com>
2023-08-08 16:32:50 -0600

    UPDATE post cards styles

--- components/main.js
+++ components/main.js
@@ -4,42 +4,16 @@ import Date from "../components/date";
 import Image from "next/image";
 
 export default function Main({ allPostsData }) {
+  console.log(allPostsData);
   return (
     <main className={styles.siteMain}>
-      {/* <section className={styles.featured}>
-        <h2>Destacados</h2>
-        <div className={styles.featuredPost}>
-          <Link href="#">
-            <img src="" alt="Featured Post 1" />
-          </Link>
-          <h3>
-            <Link href="#">Título del artículo destacado 1</Link>
-          </h3>
-          <p>Descripción del artículo destacado 1</p>
-        </div>
-        <div className={styles.featuredPost}>
-          <Link href="#">
-            <img src="" alt="Featured Post 1" />
-          </Link>
-          <h3>
-            <Link href="#">Título del artículo destacado 2</Link>
-          </h3>
-          <p>Descripción del artículo destacado 2</p>
-        </div>
-      </section> */}
-      <section className={styles.latestPosts}>
+      <section>
         <h2>Últimos artículos</h2>
         <div className={styles.postContainer}>
-          {allPostsData.map(({ id, date, title, description, image }) => (
+          {allPostsData.map(({ id, date, title, description, shareImage }) => (
             <div className={styles.post} key={id}>
               <Link href={`/posts/${id}`}>
-                <Image
-                  priority
-                  src={image}
-                  height={144}
-                  width={144}
-                  alt={title}
-                />
+                <Image src={shareImage} width={144} height={144} />
               </Link>
               <h3>
                 <Link href={`/posts/${id}`}>{title}</Link>
